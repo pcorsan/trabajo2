f563ab00e35bea6280e4e0cca44e7aa7070604af
pcorsan <pcorsan326@g.edcuaand.es>
2025-01-20 11:06:38 +0100

    aprobado

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # trabajo2
 
 trabajo 1.0
+
+trabajo 2.0
---
+++ src/main/java/es/iesjandula/trabajo2/Launcher.java
@@ -0,0 +1,12 @@
+package es.iesjandula.trabajo2;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Mundo");
+
+	}
+
+}
